182070629b1c1c277b6617bb47683e80fa458c61
Alex A. Mihaylov AKA MinimumLaw <minimumlaw@rambler.ru>
2011-11-07 20:57:32 +0400

    Apply initial patch for 2.6.35.14-ravion

--- arch/arm/mach-pxa/include/mach/colibri.h
+++ arch/arm/mach-pxa/include/mach/colibri.h
@@ -3,11 +3,191 @@
 
 #include <net/ax88796.h>
 #include <mach/mfp.h>
+#include <mach/irqs.h>
+
+
+////////////////////////////
+// Colibri PXA27x Specific
+#if defined(CONFIG_MACH_COLIBRI) 
+/*
+ * Video
+ */
+#define        COLIBRI_USE_VGA_MODULATOR
+//#undef       COLIBRI_USE_VGA_MODULATOR
 
 /*
- * common settings for all modules
+ * Backlight
  */
+#define COLIBRI_BL_MAX_INTENSITY 0xFE
+#define COLIBRI_BL_PERIOD_NS     3500
 
+/* Touchscreen Controller Philips UCB1400 */
+#define GPIO_27X_UCB1400        MFP_PIN_GPIO113
+#define COLI27X_TOUCH_IRQ       IRQ_GPIO(GPIO_27X_UCB1400)
+
+/* SecureDigital/Multimedia Card controller */
+#define GPIO_NR_COLI27X_SD_DETECT_N     MFP_PIN_GPIO0
+
+/* OHCI Host/Client detect pin */
+#define COLI27X_USB_VBUS        MFP_PIN_GPIO89
+#define COLI27X_USB_OC          MFP_PIN_GPIO88
+#define COLI27X_USB_OC_IRQ      IRQ_GPIO(COLI27X_USB_OC)
+#define COLI27X_UDC_DETECT      MFP_PIN_GPIO41
+#define COLI27X_UDC_DETECT_IRQ  IRQ_GPIO(COLI27X_UDC_DETECT)
+
+#define COLI320_UDC_DETECT      MFP_PIN_GPIO96
+#define COLI320_UDC_DETECT_IRQ  IRQ_GPIO(COLI320_UDC_DETECT)
+
+/* PCMCIA/CF device */
+#define COLI27X_PCMCIA_PRDY     MFP_PIN_GPIO1
+#define COLI27X_PCMCIA_PRST     MFP_PIN_GPIO53
+#define COLI27X_PCMCIA_PBVD1    MFP_PIN_GPIO83
+#define COLI27X_PCMCIA_PBVD2    MFP_PIN_GPIO82
+#define COLI27X_PCMCIA_PCD      MFP_PIN_GPIO84
+#define COLI27X_PCMCIA_nPPEN    MFP_PIN_GPIO107
+#define COLI27X_PCMCIA_PRDY_IRQ IRQ_GPIO(COLI27X_PCMCIA_PRDY)
+#define COLI27X_PCMCIA_PCD_IRQ  IRQ_GPIO(COLI27X_PCMCIA_PCD)
+
+extern void coli27x_init_cf_bus( void );
+extern void coli27x_exit_cf_bus( void );
+
+#define COLIBRI_PXA270_FLASH_PHYS	(PXA_CS0_PHYS)  /* Flash region */
+#define COLIBRI_PXA270_ETH_PHYS		(PXA_CS2_PHYS)  /* Ethernet */
+#define COLIBRI_PXA270_ETH_IRQ_GPIO	114
+#define COLIBRI_PXA270_ETH_IRQ		\
+	gpio_to_irq(mfp_to_gpio(COLIBRI_PXA270_ETH_IRQ_GPIO))
+
+#endif /* CONFIG_MACH_COLIBRI */
+
+////////////////////////////
+// Colibri PXA32x Specific
+#if defined( CONFIG_MACH_COLIBRI320 )
+/*
+ * PCMCIA/CompactFlash interface
+ */
+#define COLI32X_PCMCIA_PCD      mfp_to_gpio(GPIO81_GPIO)
+#define COLI32X_PCMCIA_PRDY     mfp_to_gpio(GPIO29_GPIO)
+#define COLI32X_PCMCIA_PRST     mfp_to_gpio(GPIO77_GPIO)
+#define COLI32X_PCMCIA_PBVD1    mfp_to_gpio(GPIO53_GPIO)
+#define COLI32X_PCMCIA_PBVD2    mfp_to_gpio(GPIO79_GPIO)
+#define COLI32X_PCMCIA_nPPEN    mfp_to_gpio(GPIO57_GPIO)
+#define COLI32X_PCMCIA_PCD_IRQ  gpio_to_irq( COLI32X_PCMCIA_PCD ) 
+#define COLI32X_PCMCIA_PRDY_IRQ gpio_to_irq( COLI32X_PCMCIA_PRDY ) 
+
+/*
+CPLD rgisters block       0x17800000 - 0x178FFFFF => F0000000 - F00FFFFF
+Static Memory controller  0x4A000000 - 0x4A0FFFFF => F0100000 - F01FFFFF
+*/
+
+/*
+ * CPLD map from 0x14000000 - 0x17FFFFFF : 0xEE000000 - 0xF1FFFFFF
+ */
+#define CPLD_EXT_nCS0_PHYS	(PXA3xx_CS3_PHYS)
+#define CPLD_EXT_nCS0_SIZE	0x40000
+#define CPLD_EXT_nCS0_VIRT	0xEE000000
+
+#define CPLD_EXT_nCS1_PHYS	(CPLD_EXT_nCS0_PHYS + 0x02000000)
+#define CPLD_EXT_nCS1_SIZE	0x20000
+#define CPLD_EXT_nCS1_VIRT	(CPLD_EXT_nCS0_VIRT + CPLD_EXT_nCS0_SIZE)
+
+#define CPLD_EXT_nCS2_PHYS	(CPLD_EXT_nCS1_PHYS + 0x01000000)
+#define CPLD_EXT_nCS2_SIZE	0x10000
+#define CPLD_EXT_nCS2_VIRT	(CPLD_EXT_nCS1_VIRT + CPLD_EXT_nCS1_SIZE)
+
+#define CPLD_REGS_PHYS  	(CPLD_EXT_nCS2_PHYS + 0x00800000)
+#define CPLD_REGS_SIZE		0x10000
+#define CPLD_REGS_VIRT		(CPLD_EXT_nCS2_VIRT + CPLD_EXT_nCS2_SIZE)
+
+/*
+ * CPLD for CF-Interface and External CS
+ */
+#define CPLD_REGS_P2V(X)	((X) - CPLD_REGS_PHYS + CPLD_REGS_VIRT)
+#define CPLD_REGS_V2P(X)	((X) - CPLD_REGS_VIRT + CPLD_REGS_PHYS)
+
+#ifndef __ASSEMBLY__
+#  define __CPLD_REG(x) \
+                (*((volatile u16 *)CPLD_REGS_P2V(x)))
+#else
+#  define __CPLD_REG(x)  CPLD_REGS_P2V(x)
+#endif
+
+
+// CPLD Registers and constants
+#define CPLD_EXT_nCS_CTRL_REG	__CPLD_REG( 0x17800000 )
+#define CPLD_MEM_CTRL_REG	__CPLD_REG( 0x17800004 )
+
+// CPLD_EXT_nCS_CTRL_REG
+#define EXT_nCS2_EC_DIS         (1<<15)
+#define EXT_nCS1_EC_DIS         (1<<14)
+#define EXT_nCS0_EC_DIS         (1<<13)
+#define EXT_nCS2_DIS            (1<<10)
+#define EXT_nCS1_DIS            (1<<9)
+#define EXT_nCS0_DIS            (1<<8)
+#define EXT_nCS2_EC_EN          (1<<7)
+#define EXT_nCS1_EC_EN          (1<<6)
+#define EXT_nCS0_EC_EN          (1<<5)
+#define EXT_nCS2_EN             (1<<2)
+#define EXT_nCS1_EN             (1<<1)
+#define EXT_nCS0_EN             (1<<0)
+
+// CPLD_MEM_CTRL_REG
+#define nOE_DIS                 (1<<10)
+#define RDnWR_DIS               (1<<9)
+#define CF_DIS                  (1<<8)
+#define nOE_EN                  (1<<2)
+#define RDnWR_EN                (1<<1)
+#define CF_EN                   (1<<0)
+
+
+/*
+ * Static memory controller map from 0x4A000000 - 0x4A0FFFFF : 0xED000000 - 0xED0FFFFF
+ */
+#define SMC_PHYS		0x4A000000
+#define SMC_SIZE		0x00100000
+#define SMC_VIRT		0xED000000
+
+#define SMC_REGS_P2V(X)	((X) - SMC_PHYS + SMC_VIRT)
+#define SMC_REGS_V2P(X)	((X) - SMC_VIRT + SMC_PHYS)
+
+#ifndef __ASSEMBLY__
+#  define __SMC_REG(x) \
+                (*((volatile u32 *)SMC_REGS_P2V(x)))
+#else
+#  define __SMC_REG(x)  SMC_REGS_P2V(x)
+#endif
+
+/*
+ * Static Memory Controller
+ */
+#define SMC_MSC0            __SMC_REG(0x4A000008) /* CS0 and CS1 control */
+#define SMC_MSC1            __SMC_REG(0x4A00000C) /* CS2 and CS3 control */
+#define SMC_MEMCLKCFG       __SMC_REG(0x4A000068)
+#define SMC_CSADRCFG0       __SMC_REG(0x4A000080)
+#define SMC_CSADRCFG1       __SMC_REG(0x4A000084)
+#define SMC_CSADRCFG2       __SMC_REG(0x4A000088)
+#define SMC_CSADRCFG3       __SMC_REG(0x4A00008C)
+#define SMC_CSADRCFG_P      __SMC_REG(0x4A000090)
+#define SMC_CSMSADRCFG      __SMC_REG(0x4A0000A0)
+#define SMC_MEMCLKCFG       __SMC_REG(0x4A000068)
+#define SMC_SXCNFG          __SMC_REG(0x4A00001C)
+
+/*
+ * PCMCIA/CompactFlash
+ */
+#define SMC_MECR            __SMC_REG(0x4A000014) 
+#define SMC_MCMEM           __SMC_REG(0x4A000028)
+#define SMC_MCATT           __SMC_REG(0x4A000030)
+#define SMC_MCIO            __SMC_REG(0x4A000038)
+
+#define SMC_MECR_CIT        (1 << 1)        /* Card Is There: 0 -> no card, 1 -> card inserted */
+
+extern void coli32x_init_cf_bus( void );
+extern void coli32x_exit_cf_bus( void );
+#endif /* CONFIG_MACH_COLIBRI320 */
+
+/*
+ * Colibri PXA3xx modules settings
+ */
 #if defined(CONFIG_MMC_PXA) || defined(CONFIG_MMC_PXA_MODULE)
 extern void colibri_pxa3xx_init_mmc(mfp_cfg_t *pins, int len, int detect_pin);
 #else
@@ -30,16 +210,13 @@ extern void colibri_pxa3xx_init_nand(void);
 static inline void colibri_pxa3xx_init_nand(void) {}
 #endif
 
+/*
+ * common settings for all modules
+ */
+
 /* physical memory regions */
 #define COLIBRI_SDRAM_BASE	0xa0000000      /* SDRAM region */
 
-/* definitions for Colibri PXA270 */
-
-#define COLIBRI_PXA270_FLASH_PHYS	(PXA_CS0_PHYS)  /* Flash region */
-#define COLIBRI_PXA270_ETH_PHYS		(PXA_CS2_PHYS)  /* Ethernet */
-#define COLIBRI_PXA270_ETH_IRQ_GPIO	114
-#define COLIBRI_PXA270_ETH_IRQ		\
-	gpio_to_irq(mfp_to_gpio(COLIBRI_PXA270_ETH_IRQ_GPIO))
 
 #endif /* _COLIBRI_H_ */
 
---
+++ drivers/pcmcia/pxa320_colibri.c
@@ -0,0 +1,284 @@
+/*
+ * linux/drivers/pcmcia/pxa/pxa2xx_colibri.c
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * OAO Radioavionka., 2009
+ * Alex A. Mihaylov <minimumlaw@rambler.ru>
+ *
+ * Based on:
+ * Compulab Ltd., 2003, 2007, 2008
+ * Mike Rapoport <mike@compulab.co.il>
+ *
+ */
+
+#include <linux/platform_device.h>
+#include <linux/module.h>
+#include <linux/irq.h>
+#include <linux/delay.h>
+#include <linux/gpio.h>
+
+#include <asm/mach-types.h>
+#include <asm/system.h>
+#include <mach/colibri.h>
+//#include <mach/system.h>
+#include <mach/gpio.h>
+
+#include <mach/pxa3xx-regs.h>
+#include <mach/mfp-pxa3xx.h>
+
+#include "soc_common.h"
+
+static struct pcmcia_irqs coli32x_irqs[] = {
+	{ 0, COLI32X_PCMCIA_PCD_IRQ, "PCMCIA0 CD" },
+};
+
+static int coli32x_pcmcia_hw_init(struct soc_pcmcia_socket *skt)
+{
+    int ret;
+
+    switch (skt->nr) {
+    case 0:
+	/* Ouput signals */
+	ret = gpio_request(COLI32X_PCMCIA_nPPEN, "nPPEN");
+	if (ret)
+		return ret;
+	gpio_direction_output(COLI32X_PCMCIA_nPPEN, 0); /* switch on */
+	
+	ret = gpio_request(COLI32X_PCMCIA_PRST, "PRST");
+	if (ret)
+		return ret;
+	gpio_direction_output(COLI32X_PCMCIA_PRST, 1);
+	
+	/* Input signals */
+	ret = gpio_request(COLI32X_PCMCIA_PRDY, "PRDY");
+	if (ret)
+		return ret;
+	gpio_direction_input(COLI32X_PCMCIA_PRDY);
+	
+	ret = gpio_request(COLI32X_PCMCIA_PBVD1, "PBVD1");
+	if (ret)
+		return ret;
+	gpio_direction_input(COLI32X_PCMCIA_PBVD1);
+
+	ret = gpio_request(COLI32X_PCMCIA_PBVD2, "PBVD2");
+	if (ret)
+		return ret;
+	gpio_direction_input(COLI32X_PCMCIA_PBVD2);
+        
+	// Reset PCMCIA Card
+	gpio_direction_output(COLI32X_PCMCIA_PRST, 1);
+	udelay(100);
+	gpio_direction_output(COLI32X_PCMCIA_PRST, 0);
+
+	skt->socket.pci_irq = COLI32X_PCMCIA_PRDY_IRQ;
+	ret = soc_pcmcia_request_irqs(skt, coli32x_irqs, ARRAY_SIZE(coli32x_irqs));
+	if (ret) {
+    		printk(KERN_INFO "+++ %s - irq req failed +++\n", __FUNCTION__);
+		gpio_free(COLI32X_PCMCIA_PRST);
+		gpio_free(COLI32X_PCMCIA_nPPEN);
+	};
+	break;
+    default:
+	printk(KERN_INFO "Try hw_init NOT present PCCards socket!\n");
+	ret = -ENODEV;
+    };
+    return ret;
+}
+
+static void coli32x_pcmcia_shutdown(struct soc_pcmcia_socket *skt)
+{
+    switch (skt->nr) {
+    case 0:
+	soc_pcmcia_free_irqs(skt, coli32x_irqs, ARRAY_SIZE(coli32x_irqs));
+	gpio_free(COLI32X_PCMCIA_PRST);
+	gpio_free(COLI32X_PCMCIA_nPPEN);
+	gpio_free(COLI32X_PCMCIA_PRDY);
+	gpio_free(COLI32X_PCMCIA_PBVD1);
+	gpio_free(COLI32X_PCMCIA_PBVD2);
+    default:
+	printk(KERN_INFO "Try shutdown NOT present PCCards socket!\n");
+    };
+}
+
+
+static void coli32x_pcmcia_socket_state(struct soc_pcmcia_socket *skt,
+				       struct pcmcia_state *state)
+{
+    switch (skt->nr) {
+    case 0:
+	state->detect = (gpio_get_value(COLI32X_PCMCIA_PCD) == 0)	? 0 : 1;
+	state->ready  = (gpio_get_value(COLI32X_PCMCIA_PRDY) == 0)	? 0 : 1;
+	state->bvd1   = (gpio_get_value(COLI32X_PCMCIA_PBVD1) == 0)	? 0 : 1;
+	state->bvd2   = (gpio_get_value(COLI32X_PCMCIA_PBVD2) == 0)	? 0 : 1;
+	state->vs_3v  = 1;  /* only 3,3 v */
+	state->vs_Xv  = 0;  /* no other voltage supported */
+	state->wrprot = 0;  /* not available */
+	break;
+    default:
+	printk(KERN_INFO "Try request NOT present PCCards socket!\n");
+	state->detect = 0;
+	state->ready  = 0;
+	state->bvd1   = 0;
+	state->bvd2   = 0;
+	state->vs_3v  = 0;
+	state->vs_Xv  = 0;
+	state->wrprot = 0;
+    };
+}
+
+
+static int coli32x_pcmcia_configure_socket(struct soc_pcmcia_socket *skt,
+					  const socket_state_t *state)
+{
+    unsigned long flags;
+    int ret;
+
+    // Configure socket
+    
+    local_irq_save(flags);
+    switch (skt->nr) {
+    case 0:
+        // configure Vcc and Vpp 
+        if (state->Vcc == 0 && state->Vpp == 0)
+        {
+	    gpio_set_value(COLI32X_PCMCIA_nPPEN, 1);
+	    printk(KERN_INFO "PCMCIA socket0 power disabled\n"); 
+        }
+        else if (state->Vcc == 33 && state->Vpp < 50)
+	{
+	    // PCMCIA socket0 power enabled
+	    gpio_set_value(COLI32X_PCMCIA_nPPEN, 0);
+	}
+	else
+	{
+    	    printk(KERN_ERR "%s(): unsupported Vcc %u Vpp %u combination\n",
+               __FUNCTION__, state->Vcc, state->Vpp);
+    	    return -1;
+	}
+
+	// reset PCMCIA if requested
+        if (state->flags & SS_RESET)
+	{
+	    // Reset card in slot0
+	    gpio_set_value(COLI32X_PCMCIA_PRST, 1);
+	}
+
+	if (state->flags & SS_OUTPUT_ENA)
+	{
+	    // PCCards0 output enabled
+	    coli32x_init_cf_bus();
+	}
+    
+	ret = 0;
+	break;
+    default:
+	ret = -1;    
+    };
+
+    local_irq_restore(flags);
+    udelay(200);
+    return ret;
+}
+
+static void coli32x_pcmcia_socket_init(struct soc_pcmcia_socket *skt)
+{
+    switch( skt->nr ) {
+    case 0:
+	// PCCards socket0 init (bus off, power & reset)
+	// Disable bus 
+	coli32x_exit_cf_bus();
+	// Power ON socket 
+	gpio_set_value(COLI32X_PCMCIA_nPPEN, 0);
+	// Reset socket 
+        gpio_set_value(COLI32X_PCMCIA_PRST, 1);
+        udelay(10);
+        gpio_set_value(COLI32X_PCMCIA_PRST, 0);
+	break;
+    default:
+	printk(KERN_INFO "Try init NOT present PCMCIA socket\n");
+    };
+}
+
+static void coli32x_pcmcia_socket_suspend(struct soc_pcmcia_socket *skt)
+{
+    switch( skt->nr ) {
+    case 0:
+	/* PCCards socket0 suspend (power off) */
+	gpio_set_value(COLI32X_PCMCIA_nPPEN, 1);
+	break;
+    default:
+	printk(KERN_INFO "Try suspend NOT present PCMCIA socket\n");
+    };
+}
+
+
+static struct pcmcia_low_level coli32x_pcmcia_ops __initdata = {
+	.owner			= THIS_MODULE,
+	.hw_init		= coli32x_pcmcia_hw_init,
+	.hw_shutdown		= coli32x_pcmcia_shutdown,
+	.socket_state		= coli32x_pcmcia_socket_state,
+	.configure_socket	= coli32x_pcmcia_configure_socket,
+	.socket_init		= coli32x_pcmcia_socket_init,
+	.socket_suspend		= coli32x_pcmcia_socket_suspend,
+	.nr			= 1,
+	.first			= 0, 
+};
+
+static struct platform_device *coli32x_pcmcia_device;
+
+int __init coli32x_pcmcia_init(void)
+{
+	int ret;
+
+	coli32x_pcmcia_device = platform_device_alloc("pxa32x-pcmcia", -1);
+
+	if (!coli32x_pcmcia_device)
+		return -ENOMEM;
+
+	ret = platform_device_add_data(coli32x_pcmcia_device, &coli32x_pcmcia_ops,
+				       sizeof(coli32x_pcmcia_ops));
+
+	if (ret == 0) {
+		printk(KERN_INFO "Registering Colibri PXA320 PCMCIA interface.\n");
+		ret = platform_device_add(coli32x_pcmcia_device);
+	}
+
+	if (ret) {
+		printk(KERN_INFO "Put Colibri PXA320 PCMCIA interface to platform device list.\n");
+		platform_device_put(coli32x_pcmcia_device);
+        }
+        
+	return ret;
+}
+
+void __exit coli32x_pcmcia_exit(void)
+{
+	platform_device_unregister(coli32x_pcmcia_device);
+}
+
+static int __init colibri32x_pcmcia_init(void)
+{
+	int ret = -ENODEV;
+
+	if ( cpu_is_pxa320() )
+	    ret = coli32x_pcmcia_init();
+	else
+            printk(KERN_INFO "This cpu is NOT PXA320 - skip pxa320_colibri module.\n");
+            
+	return ret;
+}
+
+static void __exit colibri32x_pcmcia_exit(void)
+{
+	coli32x_pcmcia_exit();
+}
+
+module_init(colibri32x_pcmcia_init);
+module_exit(colibri32x_pcmcia_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Alex A. Mihaylov <minimumlaw@rambler.ru>");
+MODULE_DESCRIPTION("Toradex Colibri PXA320 CF driver");
