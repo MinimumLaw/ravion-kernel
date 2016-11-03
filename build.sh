664ee5157247812e57ab1b9988909cec766c397d
Alex A. Mihaylov <alex_a_mihaylov@yahoo.com>
2016-11-03 11:45:03 +0300

    Remove unsupported HDMI CEC/Audio, fix parallel RGB display

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ if [ -z "$*" ]; then
     make ${DEF_ARGS} ${DEF_TARGET}
     echo -n Cleanup old modules...
     ${SUDO} rm -rf ${ROOT_FS_PATH}/lib/modules/* && echo OK || echo FAIL
-    ${SUDO} make ${DEF_ARGS} #  modules_install
+    ${SUDO} make ${DEF_ARGS} modules_install
     echo install kernel into rootfs and tftp
     ${SUDO} cp -f arch/arm/boot/zImage ${ROOT_FS_PATH}/boot/zImage
     ${SUDO} cp -f arch/arm/boot/dts/imx6dl-colibri-mcp.dtb ${ROOT_FS_PATH}/boot/mx6.dtb
