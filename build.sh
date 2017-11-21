46111df58ece7e75af8e259ce22f35f276df41f2
Alex A. Mihaylov <minimumlaw@rambler.ru>
2017-11-21 12:49:59 +0300

    Add CIMC Lite DTB, MCP MAX DTB, fix colibri-mcp DTB

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ export ARCH=arm
 export ROOT_FS_PATH=/cimc/root/colibri-imx6
 export BR_OVERLAY_PATH=/cimc/build/__git__/buildroot/output/overlay
 export TFTP_FS_PATH=/cimc/exporttftp
-export DEF_TARGET="zImage modules imx6dl-colibri-mcp.dtb"
+export DEF_TARGET="zImage modules imx6dl-colibri-mcp.dtb imx6dl-colibri-mcp-maximal.dtb imx6dl-colibri-cimc-lite.dtb"
 export DEF_ARGS="-j3 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} \
 INSTALL_MOD_PATH=${ROOT_FS_PATH}"
 export SUDO=sudo
@@ -19,16 +19,20 @@ if [ -z "$*" ]; then
     echo install kernel into rootfs and tftp
     ${SUDO} cp -f arch/arm/boot/zImage ${ROOT_FS_PATH}/boot/zImage
     ${SUDO} cp -f arch/arm/boot/dts/imx6dl-colibri-mcp.dtb ${ROOT_FS_PATH}/boot/mx6.dtb
+    ${SUDO} cp -f arch/arm/boot/dts/imx6dl-colibri-cimc-lite.dtb ${ROOT_FS_PATH}/boot/mx6-cimc-lite.dtb
     ${SUDO} cp -f arch/arm/boot/zImage ${TFTP_FS_PATH}/boot/zImage
     ${SUDO} cp -f arch/arm/boot/dts/imx6dl-colibri-mcp.dtb ${TFTP_FS_PATH}/boot/mx6.dtb
+    ${SUDO} cp -f arch/arm/boot/dts/imx6dl-colibri-cimc-lite.dtb ${TFTP_FS_PATH}/boot/mx6-cimc-lite.dtb
     if [ -d ${BR_OVERLAY_PATH} ]; then
 	rm -rf \
 	${BR_OVERLAY_PATH}/lib/modules/* \
 	${BR_OVERLAY_PATH}/boot/zImage  \
-	${BR_OVERLAY_PATH}/boot/mx6.dtb
-	cp -rf ${ROOT_FS_PATH}/lib/modules/*	${BR_OVERLAY_PATH}/lib/modules/
-	cp ${ROOT_FS_PATH}/boot/zImage		${BR_OVERLAY_PATH}/boot/
-	cp ${ROOT_FS_PATH}/boot/mx6.dtb 	${BR_OVERLAY_PATH}/boot/
+	${BR_OVERLAY_PATH}/boot/mx6.dtb \
+	${BR_OVERLAY_PATH}/boot/mx6-cimc-lite.dtb
+	cp -rf ${ROOT_FS_PATH}/lib/modules/*		${BR_OVERLAY_PATH}/lib/modules/
+	cp ${ROOT_FS_PATH}/boot/zImage			${BR_OVERLAY_PATH}/boot/
+	cp ${ROOT_FS_PATH}/boot/mx6.dtb 		${BR_OVERLAY_PATH}/boot/
+	cp ${ROOT_FS_PATH}/boot/mx6-cimc-lite.dtb 	${BR_OVERLAY_PATH}/boot/
     fi
 else
     make ${DEF_ARGS} $*
