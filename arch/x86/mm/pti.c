f4cf0adc3e1418f49064e3b18a2a8ec9f5837029
Alex A. Mihaylov <minimumlaw@rambler.ru>
2018-08-16 06:58:01 +0300

    Apply vanilla 4.18.0 patches

--- arch/x86/mm/pti.c
+++ arch/x86/mm/pti.c
@@ -45,7 +45,6 @@
 #include <asm/pgalloc.h>
 #include <asm/tlbflush.h>
 #include <asm/desc.h>
-#include <asm/sections.h>
 
 #undef pr_fmt
 #define pr_fmt(fmt)     "Kernel/User page tables isolation: " fmt
