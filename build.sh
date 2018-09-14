b6b9187afe4d87163df9298df21de04b3880e0bf
Alex A. Mihaylov <minimumlaw@rambler.ru>
2018-09-14 16:16:49 +0300

    Fix MTD, add MTD and STEND boards

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ export DEF_TARGET="zImage modules"
 export DEF_TARGET="${DEF_TARGET} imx6dl-colibri-mcp.dtb imx6dl-colibri-mcp-maximal.dtb imx6dl-colibri-eval-v3.dtb"
 export DEF_TARGET="${DEF_TARGET} imx6dl-colibri-cimc-lite.dtb imx6dl-colibri-cimc.dtb imx6dl-colibri-router.dtb"
 export DEF_TARGET="${DEF_TARGET} imx6dl-colibri-mtu.dtb imx6dl-colibri-pkk-m7.dtb imx6dl-colibri-pkk-m10.dtb"
+export DEF_TARGET="${DEF_TARGET} imx6dl-colibri-stend-main.dtb imx6dl-colibri-stend-tablet.dtb"
 export DEF_TARGET="${DEF_TARGET} imx6qp-ravion.dtb imx6qp-ravion-pkk-m7.dtb imx6qp-ravion-pkk-m10.dtb"
 export DEF_ARGS="-j `cat /proc/cpuinfo | grep processor | wc -l` ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} \
 INSTALL_MOD_PATH=${ROOT_FS_PATH}"
@@ -30,6 +31,8 @@ if [ -z "$*" ]; then
     ${SUDO} cp -f arch/arm/boot/dts/imx6dl-colibri-mtu.dtb ${ROOT_FS_PATH}/boot/
     ${SUDO} cp -f arch/arm/boot/dts/imx6dl-colibri-pkk-m7.dtb ${ROOT_FS_PATH}/boot/
     ${SUDO} cp -f arch/arm/boot/dts/imx6dl-colibri-pkk-m10.dtb ${ROOT_FS_PATH}/boot/
+    ${SUDO} cp -f arch/arm/boot/dts/imx6dl-colibri-stend-main.dtb ${ROOT_FS_PATH}/boot/
+    ${SUDO} cp -f arch/arm/boot/dts/imx6dl-colibri-stend-tablet.dtb ${ROOT_FS_PATH}/boot/
     ${SUDO} cp -f arch/arm/boot/dts/imx6qp-ravion.dtb ${ROOT_FS_PATH}/boot/imx6qp-ravion-test.dtb
     ${SUDO} cp -f arch/arm/boot/dts/imx6qp-ravion-pkk-m7.dtb ${ROOT_FS_PATH}/boot/
     ${SUDO} cp -f arch/arm/boot/dts/imx6qp-ravion-pkk-m10.dtb ${ROOT_FS_PATH}/boot/
@@ -43,6 +46,8 @@ if [ -z "$*" ]; then
     ${SUDO} cp -f arch/arm/boot/dts/imx6dl-colibri-mtu.dtb ${TFTP_FS_PATH}/boot/
     ${SUDO} cp -f arch/arm/boot/dts/imx6dl-colibri-pkk-m7.dtb ${TFTP_FS_PATH}/boot/
     ${SUDO} cp -f arch/arm/boot/dts/imx6dl-colibri-pkk-m10.dtb ${TFTP_FS_PATH}/boot/
+    ${SUDO} cp -f arch/arm/boot/dts/imx6dl-colibri-stend-main.dtb ${TFTP_FS_PATH}/boot/
+    ${SUDO} cp -f arch/arm/boot/dts/imx6dl-colibri-stend-tablet.dtb ${TFTP_FS_PATH}/boot/
     ${SUDO} cp -f arch/arm/boot/dts/imx6qp-ravion.dtb ${TFTP_FS_PATH}/boot/imx6qp-ravion-test.dtb
     ${SUDO} cp -f arch/arm/boot/dts/imx6qp-ravion-pkk-m7.dtb ${TFTP_FS_PATH}/boot/
     ${SUDO} cp -f arch/arm/boot/dts/imx6qp-ravion-pkk-m10.dtb ${TFTP_FS_PATH}/boot/
