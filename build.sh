a02d7ab69672f9ede5672e6fbefa3ec6ff1819a9
Alex A. Mihaylov <minimumlaw@rambler.ru>
2021-01-12 13:51:53 +0300

    Ravion-v2: add stub for router board, fixup for PCIe on cimc

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ export DTBS="${DTBS} imx51-ravion-utsvu.dtb"
 # Ravion200 quad (Ravion v2)
 export DTBS="${DTBS} imx6qp-ravion-kitsbimx6.dtb	imx6qp-ravion-cimc.dtb"
 export DTBS="${DTBS} imx6qp-ravion-mtu.dtb		imx6qp-ravion-pkk-m7.dtb"
+export DTBS="${DTBS} imx6qp-ravion-router.dtb"
 
 if [ -z ${DEFCONFIG} ]; then
     export DEFCONFIG=ravion_imx6_defconfig
