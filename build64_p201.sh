e0d3c848a18f674d9afad5ee07a281e41236e2c0
Alex A. Mihaylov <minimumlaw@rambler.ru>
2021-09-17 13:42:13 +0300

    TEMP: add amlogic meson gxbb p201 (DMYCO-BOX) support

---
+++ build64_p201.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+export ARCH=arm64
+# Name board(s) DTB file(s)
+export DTBS="${DTBS} amlogic/meson-gxbb-p201.dtb"
+
+if [ -z ${DEFCONFIG} ]; then
+    # defconfig name
+    export DEFCONFIG=meson-p201_defconfig
+fi
+if [ -z ${CROSS_COMPILE} ]; then
+    export CROSS_COMPILE=aarch64-linux-gnu-
+fi
+if [ -z ${ROOT_FS_PATH} ]; then
+    export ROOT_FS_PATH=/cimc/root/arm64-gentoo
+fi;
+if [ -z ${TFTP_FS_PATH} ]; then
+    export TFTP_FS_PATH=/cimc/exporttftp
+fi
+
+export DEF_TARGET="Image modules ${DTBS}"
+export DEF_ARGS="-j `cat /proc/cpuinfo | grep processor | wc -l` ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} \
+INSTALL_MOD_PATH=${ROOT_FS_PATH}"
+export SUDO=sudo
+
+if [ -z "$*" ]; then
+    [ -f .config ] || make ${DEF_ARGS} ${DEFCONFIG}
+    make ${DEF_ARGS} ${DEF_TARGET}
+    echo -n Cleanup old modules...
+    ${SUDO} rm -rf ${ROOT_FS_PATH}/lib/modules/* && echo OK || echo FAIL
+    ${SUDO} make ${DEF_ARGS} modules_install
+    echo install kernel into rootfs and tftp
+    ${SUDO} cp -f arch/${ARCH}/boot/Image ${ROOT_FS_PATH}/boot/Image
+    ${SUDO} cp -f arch/${ARCH}/boot/Image ${TFTP_FS_PATH}/boot/Image
+    for i in ${DTBS}; do
+	${SUDO} cp -f arch/${ARCH}/boot/dts/${i} ${ROOT_FS_PATH}/boot/
+	${SUDO} cp -f arch/${ARCH}/boot/dts/${i} ${TFTP_FS_PATH}/boot/
+    done
+else
+    make ${DEF_ARGS} $*
+fi
